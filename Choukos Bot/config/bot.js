56f25259b63fec3bfade670ebb42bb2bd1030ed5
SaohTG <76484218+SaohTG@users.noreply.github.com>
2021-01-28 22:03:17 +0100

    g

---
+++ Choukos Bot/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzkwNjQ0NTU1ODU0Nzc0Mjcy.X-DnHw.KuoPnJPAtiUhlyq0KjZ61O4LhPs',
+        prefix: '+',
+        activity: 'Mate cynthia',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
